189b3ab6fb1f04cc47e5886a124477798a24f1a9
Daniel Zürrer <daniel.zuerrer@digitecgalaxus.ch>
2024-02-06 21:41:36 +0100

    extracted volume slider

--- src/app/_components/noise.tsx
+++ src/app/_components/noise.tsx
@@ -2,29 +2,20 @@
 
 import { useEffect, useState } from "react";
 import { Card, CardContent, CardHeader, CardTitle } from "~/components/ui/card";
-import { Slider } from "~/components/ui/slider";
+import VolumeSlider from "./volumeSlider";
 
 export default function Noise({ audioFile }: { audioFile: string }) {
-  const defaultVolume = 0.33;
   const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
   const [isPlaying, setIsPlaying] = useState(false);
-  const [volume, setVolume] = useState(defaultVolume);
-
-  useEffect(() => {
-    const storedVolume = localStorage.getItem(`${audioFile}_volume`);
-    setVolume(storedVolume ? parseFloat(storedVolume) : defaultVolume);
-  }, [audioFile]);
 
   useEffect(() => {
     const loadedAudio = new Audio(audioFile);
     setAudio(loadedAudio);
     loadedAudio.loop = true;
-    console.log("loaded audio");
   }, [audioFile]);
 
   function playOrPause() {
     if (!isPlaying && audio !== null) {
-      audio.volume = volume;
       audio.play().catch((error) => {
         console.log(error);
       });
@@ -37,21 +28,10 @@ export default function Noise({ audioFile }: { audioFile: string }) {
     setIsPlaying(!isPlaying);
   }
 
-  function volumeChange(value: number[]) {
-    if (value[0] === undefined) return;
-
-    const newVolume = value[0];
-    setVolume(newVolume);
-    localStorage.setItem(`${audioFile}_volume`, newVolume.toString());
-
-    if (audio === null) return;
-
-    audio.volume = newVolume;
-  }
-
   return (
     <Card
       className={`
+        aspect-square
         max-w-xs
         bg-transparent
         text-white
@@ -61,15 +41,8 @@ export default function Noise({ audioFile }: { audioFile: string }) {
         <CardTitle>{audioFile}</CardTitle>
       </CardHeader>
       <CardContent className="h-8">
-        {isPlaying && (
-          <Slider
-            className=""
-            defaultValue={[volume]}
-            min={0}
-            max={1}
-            step={0.01}
-            onValueChange={volumeChange}
-          />
+        {isPlaying && audio && (
+          <VolumeSlider audio={audio} audioFile={audioFile} />
         )}
       </CardContent>
     </Card>
---
+++ src/app/_components/volumeSlider.tsx
@@ -0,0 +1,43 @@
+import { useEffect, useState } from "react";
+import { Slider } from "~/components/ui/slider";
+export default function VolumeSlider({
+  audio,
+  audioFile,
+}: {
+  audio: HTMLAudioElement;
+  audioFile: string;
+}) {
+  const defaultVolume = 0.33;
+
+  const [volume, setVolume] = useState<number | null>(null);
+
+  useEffect(() => {
+    const storedVolume = localStorage.getItem(`${audioFile}_volume`);
+
+    const newVolume = Number(storedVolume ?? defaultVolume);
+
+    setVolume(newVolume);
+    audio.volume = newVolume;
+  }, [audio, audioFile]);
+
+  function onVolumeChange(value: number[]) {
+    if (value[0] === undefined) return;
+
+    const newVolume = value[0];
+    audio.volume = newVolume;
+    setVolume(newVolume);
+    localStorage.setItem(`${audioFile}_volume`, newVolume.toString());
+  }
+
+  return (
+    volume && (
+      <Slider
+        defaultValue={[volume]}
+        min={0}
+        max={1}
+        step={0.01}
+        onValueChange={onVolumeChange}
+      />
+    )
+  );
+}
