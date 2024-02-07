5172e54a72785128c3d67dad997e05630624c2fc
Daniel Zürrer <daniel.zuerrer@digitecgalaxus.ch>
2024-02-07 20:36:21 +0100

    visual noise

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,9 @@ import Noise from "./_components/noise";
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-slate-900 text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-slate-900 bg-noise text-white">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-4 ">
-        <h1 className="text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
+        <h1 className="chromatic text-5xl font-extrabold tracking-tight text-white sm:text-[5rem]">
           make some noise
         </h1>
         <div className="flex w-10/12 flex-row flex-wrap justify-center gap-10">
